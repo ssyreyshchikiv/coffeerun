cc4cd057f71033db2261bb9af4c84020704aa961
ssyreyshchikov <ssyreyshchikov@mail.ru>
2018-09-06 19:32:30 +0300

    createCoffeerun

---
+++ scripts/truck.js
@@ -0,0 +1,22 @@
+(function(winndow) {
+	'use strict';
+	var App = window.App || {};
+
+	function Truck(truckId, db) {
+		this.truckId = truckId;
+		this.db = db;
+	}
+
+	Truck.prototype.createOrder = function(order) {
+		console.log('Adding order for ' + order.emailAddress);
+		this.db.add(order.emailAddress, order);
+	}
+
+	Truck.prototype.deliverOrder = function(customerId) {
+		console.log('Deliver order for ' + customerId);
+		this.db.remove(customerId);
+	}
+
+	App.Truck = Truck;
+	window.App = App;
+})(window);
